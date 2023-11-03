a1777605ddd6f6d50bb15443fe9c3e4d09f39b22
Richard Webster <Rfwebster@gmail.com>
2023-11-03 14:19:34 +1100

    refactotred xchange.py

---
+++ microscope.py
@@ -0,0 +1,168 @@
+try:
+    _online = True
+    from PyJEM import detector
+    from PyJEM import TEM3
+    print(detector.get_attached_detector())
+except ImportError:
+    _online = False
+    from PyJEM.offline import detector
+    from PyJEM.offline import TEM3
+
+stage = TEM3.Stage3()
+EOS = TEM3.EOS3()
+apt = TEM3.Apt3()
+det = TEM3.Detector3()
+def3 = TEM3.Def3()
+feg = TEM3.FEG3()
+gun = TEM3.GUN3()
+HT = TEM3.HT3()
+
+# for offline testing:
+if not _online:
+    HT.SetHtValue(300) # turn off on microsocpe!
+    gun.SetFilamentVal(3) # test use the filament, on FEG doesnt work
+    # beam valve
+    feg.SetBeamValve(1)
+    # insert detectors
+    det.SetPosition(10,1) # ADF
+    det.SetPosition(15,1) # BF
+    det.SetPosition(8,1)  # BS
+    det.SetPosition(12,1) # FScreen
+    det.SetPosition(13,1) # LScreen
+
+    # select mode
+    EOS.SelectTemStem(0)
+    EOS.SelectFunctionMode(1)
+    EOS.SetSelector(36)
+
+    # insert an aperture
+    apt.SelectKind(3)
+    apt.SetSize(2)
+
+    stage.SetPosition(100000,200000)
+
+
+# CL1_Apt = {
+#     "Index" : The index used by PyJEM to call the aperture/detector
+#     "Name" : A human readable name for the aperture/detector
+#     "Size" : A list of sizes available
+#     "Image" : the image to be shown on the ui
+#     "Xpos"  : the x position of the labels
+#     "Ypos"  : the y position of the labels
+# }
+
+"""
+Aperture Indexes:
+0:CL1, 1:CL2, 2:OL(OL Upper), 3:HC(OL Lower), 4:SA, 5:ENT, 
+6:HX, 7:BF, 8:AUX1, 9:AUX2, 10:AUX3, 11:AUX4
+"""
+
+CL1_Apt = {
+    "Index" : 0,
+    "Name" : "CL1",
+    "Size" : ["Out", "150um", "100um", "70um", "10um"],
+    "Image" : "ui/apt.png",
+    "Xpos"  : 155,
+    "Ypos"  : 100,
+}
+
+CL2_Apt = {
+    "Index" : 1,
+    "Name" : "CL2",
+    "Size" : ["Out", "50um", "40um", "30um", "20um"],
+    "Image" : "ui/apt.png",
+    "Xpos"  : 155,
+    "Ypos"  : 150,
+}
+
+OL_Apt = {
+    "Index" : 3,
+    "Name" : "OL",
+    "Size" : ["Out", "120um", "60um", "20um", "5um"],
+    "Image" : "ui/apt.png",
+    "Xpos"  : 155,
+    "Ypos"  : 290,
+}
+
+SA_Apt = {
+    "Index" : 4,
+    "Name" : "SA",
+    "Size" : ["Out", "100um", "50um", "20um", "10um"],
+    "Image" : "ui/apt.png",
+    "Xpos"  : 155,
+    "Ypos"  : 330,
+}
+
+HX_Apt = {
+    "Index" : 6,
+    "Name" : "HX",
+    "Size" : ["Out", "500um", "200um"],
+    "Image" : "ui/apt.png",
+    "Xpos"  : 155,
+    "Ypos"  : 200,
+}
+
+BF_Apt = {
+    "Index" : 7,
+    "Name" : "BF",
+    "Size" : ["Out", "5-2.5mm", "3mm", "2mm", "1mm"],
+    "Image" : "ui/apt.png",
+    "Xpos"  : 155,
+    "Ypos"  : 600,
+}
+
+aperture_list = [CL1_Apt, CL2_Apt, OL_Apt, SA_Apt, HX_Apt, BF_Apt]
+
+"""
+Detector Configurations:
+1:SEI, 2:EDS, 3:BI-PRISM, 4:DFI-EF, 5:TVCAM-U, 6:SSCAM-U, 7:FARADAY-CAGE, 8:BS, 9:HRD, 
+10:DFI-U, 11:BFI-U, 12:SCR-F, 13:SCR-L, 14:DFI-B, 15:BFI-B, 16:SSCAM-B,17:TVCAM-B, 18:EELS, 19:TVCAM-GIF, 
+20:SSCAM-GIF, 21:BEI-TOPO, 22:BEI-COMPO, 23:SEI-TOPO, 24:SEI-COMPO
+"""
+
+BS_Det = {
+    "Index" : 8,
+    "Name" : "BS",
+    "State" : ["Out", "In"],
+    "Image" : "ui/bs.png",
+    "Xpos"  : 155,
+    "Ypos"  : 580,
+}
+
+ADF_Det = {
+    "Index" : 10,
+    "Name" : "ADF",
+    "State" : ["Retracted", "Inserted"],
+    "Image" : "ui/adf.png",
+    "Xpos"  : 155,
+    "Ypos"  : 400,
+}
+
+BF_Det = {
+    "Index" : 11,
+    "Name" : "BF",
+    "State" : ["Retracted", "Inserted"],
+    "Image" : "ui/bf.png",
+    "Xpos"  : 155,
+    "Ypos"  : 640,
+}
+
+LScreen_Det = {
+    "Index" : 13,
+    "Name" : "LScreen",
+    "State" : ["Raised", "Lowered"],
+    "Image" : "ui/lscreen.png",
+    "Xpos"  : 155,
+    "Ypos"  : 490,
+}
+
+FScreen_Det = {
+    "Index" : 12,
+    "Name" : "FScreen",
+    "State" : ["Retracted", "Inserted"],
+    "Image" : "ui/fscreen.png",
+    "Xpos"  : 155,
+    "Ypos"  : 460,
+}
+
+detector_list = [BS_Det, ADF_Det, BF_Det, LScreen_Det, FScreen_Det]
--- xchange.py
+++ xchange.py
@@ -1,103 +1,180 @@
-_online = True
-try:
-    from PyJEM import TEM3, detector
-except ImportError: 
-    _online = False
-    from PyJEM.offline import TEM3, detector
-
 import tkinter as tk
 from tkinter import ttk
+import microscope as GrandARM
 
+# User defined variables
 POLLING_TIME = 2000 # ms
 
-class Xchange():
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.title('Xchange')
-        self.root.geometry('300x300')
-        self.root.resizable(False, False)
-        self.root.configure(background='black')
-        self.root.attributes('-topmost', True)
+class WarningPopup(tk.Toplevel):
+    def __init__(self, parent, message):
+        super().__init__(parent)
+        self.title("Warning")
+        self.attributes('-topmost', True)
+        self.geometry("500x100")
+        self.configure(background="yellow")
+        self.message = ttk.Label(self, text=message,
+                                 font=("Arial", 12, "bold"), justify=tk.CENTER,
+                                 background="yellow", foreground="black")
+        self.message.pack(pady=10)
+        self.ok_button = tk.Button(self, text="OK", width=20, command=self.destroy)
+        self.ok_button.pack(pady=10)
 
-        self.stage = TEM3.Stage3()
-        self.feg = TEM3.FEG3()
-        self.apt = TEM3.Apt3()
+class Xchange(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Xchange')
+        self.geometry('300x300')
+        self.resizable(False, False)
+        self.configure(background='black')
+        self.attributes('-topmost', True)
 
         self.setup_ui()
 
         self.update()
-        self.root.mainloop()
+        self.mainloop()
 
 
     def setup_ui(self):
+        self.style = ttk.Style()
+        self.style.theme_use('vista')
+        self.style.configure("TFrame", background="black")
+
+        self.style.configure("TButton", foreground="black", background="green", font=('Arial', 12, 'bold'), borderwidth=1)
+        self.style.map("TButton", background=[("disabled", "light grey"), ("active", "green")], foreground=[("disabled", "grey"), ("active", "black")])
+
+        self.style.configure("TLabel", foreground="light green", background="black", font=('Arial', 12, 'bold'))
+        self.style.configure("TCheckbutton", foreground="light green", background="black", font=('Arial', 12, 'bold'))
+
         title_font = ('Arial', 16, 'bold')
         font = ('Arial', 12)
 
-        self.frame = tk.Frame(self.root, bg='black')
-        self.frame.pack(padx=5, pady=5, fill=tk.X, expand=True, anchor='n')
+        self.frame = ttk.Frame(self)
+        self.frame.pack(padx=5, pady=5, fill=tk.X, expand=True, anchor='center')
+        
+        self.beam_value_label = ttk.Label(self.frame, text='Beam Valve: ')
+        self.beam_value_label.pack(padx=5, pady=5,  fill=tk.BOTH, expand=True)
 
-        self.beam_value_label = tk.Label(self.frame, text='Beam Valve: ', 
-                                         font=title_font, bg='black', fg='light green')
-        self.beam_value_label.grid(row=0, column=0, sticky='nsew')
+        self.holder_status_label = ttk.Label(self.frame, text='Holder: ')
+        self.holder_status_label.pack(padx=5, pady=5,  fill=tk.BOTH, expand=True)
 
-        self.holder_status_label = tk.Label(self.frame, text='Holder: ', 
-                                            font=title_font, bg='black', fg='light green')
-        self.holder_status_label.grid(row=1, column=0, sticky='nsew')
+        self.xy_stage_label = ttk.Label(self.frame, text='Stage: X= ,  Y= , Z= ')
+        self.xy_stage_label.pack(padx=5, pady=5,  fill=tk.BOTH, expand=True)
 
-        self.stage_label = tk.Label(self.frame, text='Stage: X= ,  Y= , Z= ', 
-                                    font=font, bg='black', fg='light green')
-        self.stage_label.grid(row=2, column=0, sticky='nsew')
+        self.z_stage_label = ttk.Label(self.frame, text='Z= ')
+        self.z_stage_label.pack(padx=5, pady=5,  fill=tk.BOTH, expand=True)
 
-        self.tilt_label = tk.Label(self.frame, text='Tilt: Alpha= , Beta= ', 
-                                   font=font, bg='black', fg='light green')
-        self.tilt_label.grid(row=3, column=0, sticky='nsew')
+        self.tilt_label = ttk.Label(self.frame, text='Tilt: Alpha= , Beta= ')
+        self.tilt_label.pack(padx=5, pady=5,  fill=tk.BOTH, expand=True)
 
-        self.xchange_button = tk.Button(self.root, text='Exchange', font=title_font, 
-                                        bg='light green', fg='black', borderwidth=10, 
-                                        command=self.set_xchange)
+        self.xchange_button = ttk.Button(self, text='Exchange')
         self.xchange_button.pack(padx=5, pady=5,  fill=tk.BOTH, expand=True)
+
+        self.ontop_checkbox = ttk.Checkbutton(self, text='Always on top', state=tk.NORMAL,
+                                             command=self.toggle_ontop)
+        self.ontop_checkbox.state(['!selected']) # has no effect
+        self.ontop_checkbox.pack(padx=5, pady=5,  fill=tk.BOTH, expand=True)
+        self.update_idletasks()
+
+    def toggle_ontop(self):
+        if self.attributes('-topmost'):
+            self.attributes('-topmost', False)
+        else:
+            self.attributes('-topmost', True)
+        self.update_idletasks()
 
 
     def update(self):
+        # check if the beam valve is closed
+        if GrandARM.feg.GetBeamValve():
+            self.beam_valve = "Open" 
+            self.beam_value_label.configure(foreground='red')
+        else:
+            self.beam_valve = "Closed"
+            self.beam_value_label.configure(foreground='light green')
+        self.beam_value_label.configure(text='Beam Valve: {}'.format(self.beam_valve))
 
-        beam_valve = "Closed"
-        if self.feg.GetBeamValve():
-            beam_valve = "Open" 
-        self.beam_value_label.configure(text='Beam Valve: {}'.format(beam_valve))
+        if GrandARM.stage.GetHolderStts():
+            self.holder_status = "IN"
+            self.holder_status_label.configure(foreground='light green')
+        else: 
+            self.holder_status = "OUT"
+            self.holder_status_label.configure(foreground='red')
+            
+        self.holder_status_label.configure(text='Holder: ' + self.holder_status)
 
-        holder_status = "OUT"
-        if self.stage.GetHolderStts():
-            holder_status = "IN"
-        self.holder_status_label.configure(text='Holder: ' + holder_status)
+        self.pos = GrandARM.stage.GetPos()
+        self.xy_stage_label.configure(text='Stage: X= {0:.3f}, Y= {1:.3f}'.format(self.pos[0]/1000, self.pos[1]/1000))
+        self.z_stage_label.configure(text='Z= {0:.3f}'.format(self.pos[2]/1000))
+        self.tilt_label.configure(text='Tilt: Alpha= {0:.3f}, Beta= {1:.3f}'.format(self.pos[3], self.pos[4]))
 
-        pos = self.stage.GetPos()
-        self.stage_label.configure(text='Stage: X= {0:.3f}, Y= {1:.3f}, Z= {2:.3f}'.format(pos[0], pos[1], pos[2]))
-        self.tilt_label.configure(text='Tilt: Alpha= {0:.3f}, Beta= {1:.3f}'.format(pos[3], pos[4]))
+        # if the stage is out of bounds set the relative color to be red
+        if any([p >= 2. for p in self.pos[:2]]):
+            self.xy_stage_label.configure(foreground='red')
+            self.z_stage_label.configure(foreground='red')
+        else:
+            self.xy_stage_label.configure(foreground='light green')
+            self.z_stage_label.configure(foreground='light green')
 
-        self.root.after(POLLING_TIME, self.update)
+        if any([t >= 1. for  t in self.pos[3:]]):
+            self.tilt_label.configure(foreground='red')
+        else:
+            self.tilt_label.configure(foreground='light green')
+
+
+        # if the holder is out disable the button so people can't neutralise stage
+        if self.holder_status == "OUT":
+            self.xchange_button.configure(state=tk.DISABLED)
+        # else if the beam valve is closed and the stage values are between 0 and 1
+        # then set the color to be green 
+        elif self.beam_valve == "Closed" and all(0 <= x <= 1 for x in self.pos):
+            self.xchange_button.configure(state=tk.NORMAL)
+        # else if the beam valve is open and the stage values are greater than 1
+        # then set the color to be red
+        elif self.beam_valve == "Open" and any(x > 1 for x in self.pos):
+            self.xchange_button.configure(state=tk.NORMAL)
+
+
+        self.after(POLLING_TIME, self.update)
+        self.update_idletasks()
 
     def set_xchange(self):
+        # Create a pop-up window with a message
+        popup = WarningPopup(self, "Make sure EDX detectors are in the S-CHANGE position!")
+        popup.wait_window()
+        print("EDX warning acknowledged")
 
-        self.feg.SetBeamValve(1)
+        GrandARM.feg.SetBeamValve(0)
         
-        self.stage.Stop()
-        self.root.after(1000)
-        self.stage.SetPosition(0, 0, 0, 0, 0)
-
-        for i in range(0, 5):
-            self.remove_aperture(i)
+        GrandARM.stage.Stop()
+        print("stage stopped")
+        while any(GrandARM.stage.GetStatus()):
+            # wait for stage to stop moving
+            print("stage is moving")
+            pass
+        GrandARM.stage.SetOrg()
+        print("stage neutralised")
+        while any(GrandARM.stage.GetStatus()):
+            # wait for stage to stop moving
+            print("stage is moving")
+            pass
+        # wait for stage to stop moving
+        for apt in GrandARM.aperture_list:
+            # could implement a more sofisicated method to determine which apertures are allowed
+            print("removing aperture {}".format(apt["Name"]))
+            self.remove_aperture(apt["Index"])
 
     def remove_aperture(self, kind):
+        
         try:
-            self.apt.SetExpSize(kind,0)
+            if GrandARM.apt.GetExpSize(kind):
+                GrandARM.apt.SetExpSize(kind,0)
         except:
-            self.apt.SelectKind(kind)
-            self.apt.SetSize(0)
+            if GrandARM.apt.GetSize(kind):
+                GrandARM.apt.SelectKind(kind)
+                GrandARM.apt.SetSize(0)
+                self.after(3000)
 
-
-
-
-
+        self.update_idletasks()
 
 
 if __name__ == '__main__':
